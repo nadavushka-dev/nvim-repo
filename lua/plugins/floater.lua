c929a0aedb92d93911ecc4941b4d42609aec9dd8
nadavushka-dev <nadavushkadev@gmail.com>
2025-11-05 19:45:38 +0200

    fix floater

--- lua/plugins/floater.lua
+++ lua/plugins/floater.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    dir = "~/dotfiles/config/nvim/lua/floater.nvim/",
-    config = function()
-      local floater = require("floater")
-      vim.keymap.set({ 'n', 't' }, "<leader>tt", floater.terminal)
-      vim.keymap.set('n', "<leader>sk", floater.sketch)
-    end
-  },
+	{
+		dir = "~/nvim_repo/lua/floater.nvim/",
+		config = function()
+			local floater = require("floater")
+			vim.keymap.set({ "n", "t" }, "<leader>tt", floater.terminal)
+			vim.keymap.set("n", "<leader>sk", floater.sketch)
+		end,
+	},
 }
